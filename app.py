a598f3b64cf3dc7dfa82317deb4520a4b7e6e7bd
jmc1367 <jmc1367@nyu.edu>
2019-11-16 18:15:52 -0700

    initialized success_message in login route
    Signed-off-by: jmc1367 <jmc1367@nyu.edu>

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def register():
 def login():
     if current_user.is_authenticated:
         success_message = 'Success'
+        return render_template('login.html', title='Login', form=form, result=success_message)
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -74,11 +75,13 @@ def login():
             session['id'] = login_history.id
             session['username'] = login_history.username
             success_message = 'Success'
+            return render_template('login.html', title='Login', form=form, result=success_message)
         else:
             success_message = 'Failure'
+            return render_template('login.html', title='Login', form=form, result=success_message)
     if request.method == 'GET':
         success_message = ''
-    return render_template('login.html', title='Login', form=form, result=success_message)
+        return render_template('login.html', title='Login', form=form, result=success_message)
 
 @app.route("/history")
 @login_required
